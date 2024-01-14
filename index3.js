43782d71d98b0bbfd6125ee70600fbf8fb9dd40a
Afaque Ahmad <afaque3000@gmail.com>
2024-01-14 18:58:50 +0530

    correcting

--- index3.js
+++ index3.js
@@ -1,96 +1,92 @@
-// console.log(Math.random());
-// console.log(Math.PI);
-// console.log(Math.round(1.8));
-// console.log(Math.max(1,3,-5,1));
-// console.log(Math.min(1,3,-1,5));
-// console.log(Math.abs(-2.10));
+function walk(){
+    console.log('walking');
+}
+walk();
+let fun1=function move(){
+    console.log('moving');
+}
+let fun2=fun1;
+fun2();
 
+function sum(){
+     console.log(arguments);
+     
+     let total=0;
+     for(let value of arguments){
+        total+=value;
+     }
+     return total;
+    
 
-let lastName=' Ahmad ';
-let firstName=new String('Afaque');
-console.log(typeof lastName);
-console.log(typeof firstName);
-console.log(firstName);
-// . lagate hi lastName(primitive) is treated as an object(referance)   
-console.log(lastName.length);
+}
+console.log(sum(1,2,3,4,5));
 
-let str="This is my first message";
-let s=str.split(' ');
-console.log(s);
-let jaise=`  This is
-  ${lastName}
-2nd mesaage.`;
-console.log(jaise);
+function abc(...args){
+    let total=0;
+    console.log(args);
+    for(let value of args){
+        total+=value;
+    }
+    return total;
+}
+console.log(abc(1,2,3,4,5,6,7,8,9,10));
+//getter setter
+let person={
+    fName:'Afaque',
+    lName:'Ahmad',
+    get fullName(){
+       return `${person.fName} ${person.lName}`;
+    },
 
-let date=new Date(1994,11,21,21);
-date.setFullYear(1998);
-console.log(date);
+    set fullName(value){
+        let parts=value.split(' ');
+        this.fName=parts[0];
+        this.lName=parts[1];
+    }
+    
+    
+};
 
-let arr=[1,2,3,4];
-arr.push('Ahmad');
-arr.unshift('Afaque');
-arr.splice(3,0,'+');
-arr.pop();
-console.log(arr);
-
-let subjects=[
-    {subject:'Hindi',  totalMarks:100,marksObtained:90},
-    {subject:'English',totalMarks:100,marksObtained:85},
-    {subject:'Maths',  totalMarks:100,marksObtained:95},
-    {subject:'Science',totalMarks:100,marksObtained:80}
-]
-
-   /* let box=subjects.find(
-        function(obj){
-         return   obj.subject==='English';
-
+person.fullName='Aditya Shah';
+console.log(person.fullName);
+// error handling
+let person2={
+    fName:'Afaque',
+    lName:'Ahmad',
+    get fullName(){
+        return `${fName} ${lName}`;
+    },
+    set fullName(value){
+        if(typeof(value) !== String){
+        throw new Error ('string daal be');
+        
         }
-    )   */
-    let box=subjects.find(obj=>obj.subject==='Maths');
-    console.log(box);
-let numbers=[1,2,3,4,5];
-numbers.pop();
-numbers.shift();
-numbers.splice(1,1);
-console.log(numbers);
-let first=[1,2,3];
-let second=[4,5,6];
-let combined=first.concat(second);
-console.log(combined);
+        let parts=value.split(' ');
+        fName=parts[0];
+        lName=parts[1];
 
-let slicedItem=combined.slice(2,4);
-console.log(slicedItem);
+    }
+}
+try{
+    person2.fullName=true;
+}
+catch(e){
+    // alert(e);
 
-let ar=[10,20,3,4,15,-41,76];
-// for(let value of ar){
-//     
-// }
-// ar.forEach(function(value){
-//     console.log(value);
-// })
-// ar.forEach(value=>console.log(value))
+}
 
-let mess='My name is Khan';
-let mess2=mess.split(' ');
-console.log(mess2);
+// reduce function
+let arr7=[1,2,3,4,5,6,7,8];
 
-let mess3=mess2.join();
-console.log(mess3);
-
-let nbr = [7, 5, -4, 50, 6, 0,"3","1","19"];
+let totalSum=arr7.reduce((accumulator, currentVal)=> accumulator + currentVal,0);
+console.log('totalSum : '+ totalSum);
 
 
-nbr.sort((a,b)=>a-b);
 
-console.log(nbr);
 
-let filtered=nbr.filter(value=>value>=0);
- 
-console.log(filtered);
 
-let arrr=[1,2,-3,4,-5,6];
-let boxy=arrr.map(value=>'student no:'+ value);
-console.log(boxy);
 
-let items=arrr.filter(num => num >= 0).map(val =>  obj={value:val});
-console.log(items);
+
+
+
+
